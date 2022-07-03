a8bc7e05033510fc1ecaee914bfdd5223f61bcd7
Alexander Volkov <AV-html@yandex.ru>
2022-07-03 19:48:15 +0300

    add(Main): create Main component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import main from './Main.module.css'
+
+export function Main() {
+    return (
+        <main className={main.main}>
+            Main
+        </main>
+    );
+}
