2c9e7f81aec547102bf439b80d7e8f30759d950a
Alexander Volkov <AV-html@yandex.ru>
2022-07-04 11:00:52 +0300

    add(Footer): ready footer + styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,12 +2,64 @@ import React from 'react';
 
 import styles from './../../common/styles/small-container.module.css'
 import footer from './Footer.module.css'
+import blocks from './../../common/styles/blocks.module.css'
+
+
+import {SvgHH} from '../svg/SvgHH';
+import {SvgGithub} from '../svg/SvgGithub';
+import {SvgTm} from '../svg/SvgTm';
+import {SvgVk} from '../svg/SvgVk';
+
 
 export function Footer() {
     return (
         <footer className={footer.footer}>
             <div className={styles.container}>
-                (c) 12341
+                <div className={footer.name}>
+                    <span>Alexander</span> Volkov
+                </div>
+                <ul className={footer.socials}>
+                    <li className={footer.item}>
+                        <a href="https://hh.ru"
+                           className={footer.link}
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                            <SvgHH/>
+                        </a>
+                    </li>
+                    <li className={footer.item}>
+                        <a href="https://t.me/AV_html"
+                           className={footer.link}
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                            <SvgTm/>
+                        </a>
+                    </li>
+                    <li className={footer.item}>
+                        <a href="https://vk.com/avtofront"
+                           className={footer.link}
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                            <SvgVk/>
+                        </a>
+                    </li>
+                    <li className={footer.item}>
+                        <a href="https://github.com/AV-html"
+                           className={footer.link}
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                            <SvgGithub/>
+                        </a>
+
+                    </li>
+                </ul>
+                <p className={`${footer.copy} ${blocks.text}`}>
+                    Copyright © 2022 All rights reserved
+                </p>
             </div>
         </footer>
     );
