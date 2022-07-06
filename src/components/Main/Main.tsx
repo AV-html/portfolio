7f5cee011a5646819e9c8e4ebb24091cd89849e1
Alexander Volkov <AV-html@yandex.ru>
2022-07-06 15:51:26 +0300

    add(Start): first hello section

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import main from './Main.module.css'
+import {Start} from './Start/Start';
 
 export function Main() {
     return (
         <main className={main.main}>
-            Main
+            <Start/>
         </main>
     );
 }
---
+++ src/components/Main/Start/Start.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import start from './Start.module.css'
+import holder from './../../../common/styles/container.module.css'
+
+import avatar from './../../../static/480x640.png'
+import {ButtonLink} from '../../ButtonLink/ButtonLink';
+
+export function Start() {
+    return (
+        <section id={'main'} className={start.start}>
+            <div className={`${holder.container} ${start.row}`}>
+                <div className={start.body}>
+                    <h1 className={start.heading}>
+                        <span className={start.name}>Alexander&#160;Volkov</span> <span
+                        className={start.dev}>Front-end&#160;Developer</span>
+                    </h1>
+                    
+                    <ButtonLink href={'#contacts'}
+                                className={start.link}
+                    >
+                        Contact with me
+                    </ButtonLink>
+
+                </div>
+
+                <div className={start.image}>
+                    <img src={avatar} alt="Alexander Volkov"/>
+                </div>
+            </div>
+        </section>
+    );
+}
